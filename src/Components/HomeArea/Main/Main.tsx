00791f461d058f2f27445a642ed5ccde1970edf9
Lidor Argaman <lidorargaman@gmail.com>
2021-07-05 18:44:02 +0300

    changed the main

--- src/Components/HomeArea/Main/Main.tsx
+++ src/Components/HomeArea/Main/Main.tsx
@@ -5,7 +5,7 @@ import "./Main.css";
 function Main(): JSX.Element {
     return (
         <div className="Main">
-            {/* <img src="https://media.giphy.com/media/r1IMdmkhUcpzy/giphy.gif" /> */}
+            <img src="https://media.giphy.com/media/r1IMdmkhUcpzy/giphy.gif" />
             <Total/>
             <Workout/>
         </div>
