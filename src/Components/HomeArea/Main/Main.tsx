2f69fda41131079ab9e37d9f71b541c9e9df5108
Lidor Argaman <lidorargaman@gmail.com>
2021-07-05 12:19:09 +0300

    create home area

---
+++ src/Components/HomeArea/Main/Main.tsx
@@ -0,0 +1,13 @@
+import Total from "../Total/Total";
+import "./Main.css";
+
+function Main(): JSX.Element {
+    return (
+        <div className="Main">
+            {/* <img src="https://media.giphy.com/media/r1IMdmkhUcpzy/giphy.gif" /> */}
+            <Total/>
+        </div>
+    );
+}
+
+export default Main;
---
+++ src/Components/HomeArea/Total/Total.tsx
@@ -0,0 +1,13 @@
+import "./Total.css";
+
+function Total(): JSX.Element {
+    const numOfGoals = 801;
+    return (
+        <div className="Total Box">
+            <h2>Goals</h2>
+            <p>Cristiano scored {numOfGoals} so far</p>
+        </div>
+    );
+}
+
+export default Total;
